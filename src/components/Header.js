137cea922db76068b9416f70c8d9cee8b2674f7b
Valeria Protsan <valeriyapro@gmail.com>
2018-04-30 13:36:10 -0400

    breaking up into sep file done

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Stats from './Stats';
+import Stopwatch from './Stopwatch';
+
+// Move to components/Header.js
+// ----------------------------------------------
+const Header = props => {
+  return (
+    <div className="header">
+      <Stats players={props.players} />
+      <h1>Scoreboard</h1>
+      <Stopwatch />
+    </div>
+  );
+}
+
+Header.propTypes = {
+  players: PropTypes.array.isRequired
+};
+
+export default Header;
